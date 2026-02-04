63332202707e73aafe156d813048a4b1ad0c13f6
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2026-02-04 13:20:35 +0530

    desktop gone

--- apps/desktop/src/routes/_mainLayout/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_mainLayout/")({
-  component: App,
-});
-
-function App() {
-  return <div className="text-center">hello world</div>;
-}
