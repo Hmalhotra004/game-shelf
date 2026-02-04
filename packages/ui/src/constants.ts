29c0df0437606df537a03b48df58af5abc6285c2
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2026-02-04 12:17:28 +0530

    backend url

--- packages/ui/src/constants.ts
+++ packages/ui/src/constants.ts
@@ -1,2 +1,4 @@
 export const BASEURL =
-  process.env.NODE_ENV === "development" ? "http://localhost:8080" : "";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:8080"
+    : "https://game-shelf-m216.onrender.com";
