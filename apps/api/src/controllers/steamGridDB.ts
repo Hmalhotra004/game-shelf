03c290aba518e1d3443a9caaf95491fbc00cb4a9
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2026-02-18 13:17:08 +0530

    Update steamGridDB.ts

--- apps/api/src/controllers/steamGridDB.ts
+++ apps/api/src/controllers/steamGridDB.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import SGDB, { SGDBGame } from "steamgriddb";
+import SGDB from "steamgriddb";
 
 export const linkGame = async (req: Request, res: Response) => {
   try {
@@ -26,38 +26,38 @@ export const linkGame = async (req: Request, res: Response) => {
 
 export const getGrids = async (req: Request, res: Response) => {
   try {
-    const { name, steamGridDBId, steamAppId } = req.query as {
-      name: string | null;
-      steamAppId: string | null;
-      steamGridDBId: string | null;
+    const { steamGridDBId, steamAppId } = req.query as {
+      steamAppId?: string;
+      steamGridDBId?: string;
     };
 
-    if (!name?.trim())
-      return res.status(400).json({ error: "Name is required" });
+    if (!steamAppId && !steamGridDBId) {
+      return res.status(400).json({ error: "At least one ID is required" });
+    }
 
     const client = new SGDB({
       key: process.env.STEAM_GRID_DB_API_KEY!,
       baseURL: "https://www.steamgriddb.com/api/v2",
     });
 
-    let games: SGDBGame[] = [];
+    let type: "steam" | "game";
+    let id: number;
 
-    if (!steamGridDBId && !steamAppId) {
-      games = await client.searchGame(name.toLowerCase());
+    if (steamGridDBId) {
+      type = "game";
+      id = Number(steamGridDBId);
+    } else {
+      type = "steam";
+      id = Number(steamAppId);
     }
 
-    const gridId = games.find(
-      (game) => game.name === name.toLocaleLowerCase(),
-    )?.id;
-
     const grids = await client.getGrids({
-      type: "game",
-      id: gridId!,
-      page: 1,
+      type,
+      id,
       dimensions: ["600x900"],
     });
 
-    res.status(200).json({ games, grids });
+    res.status(200).json(grids);
   } catch (e) {
     console.error(e);
     res.status(500).json({
@@ -66,4 +66,44 @@ export const getGrids = async (req: Request, res: Response) => {
   }
 };
 
-export const getHeros = async (req: Request, res: Response) => {};
+export const getHeros = async (req: Request, res: Response) => {
+  try {
+    const { steamGridDBId, steamAppId } = req.query as {
+      steamAppId?: string;
+      steamGridDBId?: string;
+    };
+
+    if (!steamAppId && !steamGridDBId) {
+      return res.status(400).json({ error: "At least one ID is required" });
+    }
+
+    const client = new SGDB({
+      key: process.env.STEAM_GRID_DB_API_KEY!,
+      baseURL: "https://www.steamgriddb.com/api/v2",
+    });
+
+    let type: "steam" | "game";
+    let id: number;
+
+    if (steamGridDBId) {
+      type = "game";
+      id = Number(steamGridDBId);
+    } else {
+      type = "steam";
+      id = Number(steamAppId);
+    }
+
+    const heros = await client.getHeroes({
+      type,
+      id,
+      dimensions: ["1920x620", "3840x1240"],
+    });
+
+    res.status(200).json(heros);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      error: "An error occurred while fetching data from SteamGridDB.",
+    });
+  }
+};
