914d5500ae5e03a97143a2a95eaa3e4118e2e69d
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2026-04-05 19:01:47 +0530

    actor(client-db): remove package and migration sources

--- packages/client-db/src/adapters/expo.ts
+++
@@ -1,8 +0,0 @@
-// import * as SQLite from "expo-sqlite";
-// import { drizzle } from "drizzle-orm/expo-sqlite";
-// import * as schema from "../schema";
-
-// export function createExpoDB() {
-//   const db = SQLite.openDatabaseSync("gameshelf.db");
-//   return drizzle(db, { schema });
-// }
