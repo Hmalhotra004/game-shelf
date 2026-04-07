91c9716de90d333958559c0cb53f181404c7a7e4
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2026-04-07 15:24:42 +0530

    feat(router add demo/table & improve query

--- apps/web/src/integrations/tanstack-query/root-provider.tsx
+++ apps/web/src/integrations/tanstack-query/root-provider.tsx
@@ -1,10 +1,29 @@
-import { QueryClient } from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export function getContext() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 15,
+        gcTime: 1000 * 60 * 60 * 30,
+        retry: 3,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
   return {
     queryClient,
-  }
+  };
 }
-export default function TanstackQueryProvider() {}
+export default function TanstackQueryProvider({
+  children,
+  queryClient,
+}: Readonly<{
+  children: React.ReactNode;
+  queryClient: QueryClient;
+}>) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
