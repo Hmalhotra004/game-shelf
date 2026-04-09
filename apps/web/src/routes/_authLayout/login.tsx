077a203ee231eb6ed19defe2a6396825b2b3ebf9
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2026-04-09 12:53:32 +0530

    feat(auth): add login route and responsive login form

---
+++ apps/web/src/routes/_authLayout/login.tsx
@@ -0,0 +1,135 @@
+import AlertError from "@/components/AlertError";
+import { FormInput, FormInputPassword } from "@/components/form/Form";
+import { Button } from "@/components/ui/button";
+import { FieldDescription, FieldGroup } from "@/components/ui/field";
+import { Separator } from "@/components/ui/separator";
+import { Spinner } from "@/components/ui/spinner";
+import { authClient } from "@/lib/authClient";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { loginSchema } from "@repo/schemas/schemas/auth";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import z from "zod";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+import {
+  createFileRoute,
+  Link,
+  redirect,
+  useNavigate,
+} from "@tanstack/react-router";
+
+type FormData = z.infer<typeof loginSchema>;
+
+export const Route = createFileRoute("/_authLayout/login")({
+  beforeLoad: async () => {
+    const session = await authClient.getSession();
+
+    if (session.data?.session.id && session.data?.user.id) {
+      throw redirect({ to: "/", replace: true });
+    }
+  },
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const navigate = useNavigate();
+
+  const onSuccess = () => {
+    navigate({ to: "/", replace: true });
+  };
+
+  const [error, setError] = useState<string | null>(null);
+  const [pending, setPending] = useState(false);
+
+  const form = useForm<FormData>({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: FormData) {
+    setError(null);
+    setPending(true);
+
+    await authClient.signIn.email(
+      {
+        email: values.email,
+        password: values.password,
+      },
+      {
+        onSuccess: () => {
+          onSuccess();
+        },
+        onError: ({ error }) => {
+          setError(error.message);
+        },
+        onResponse: () => {
+          setPending(false);
+        },
+      },
+    );
+  }
+
+  return (
+    <Card className="w-full h-full md:w-121.5 max-sm:w-85 sm:w-100">
+      <CardHeader className="flex items-center justify-center text-center px-7 pt-1">
+        <CardTitle className="text-2xl">Welcome back!</CardTitle>
+      </CardHeader>
+
+      <div className="px-7">
+        <Separator />
+      </div>
+
+      <CardContent className="px-7">
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup className="gap-4">
+            <FormInput
+              name="email"
+              control={form.control}
+              type="email"
+              autoComplete="email"
+              placeholder="Email"
+              disabled={pending}
+            />
+
+            <FormInputPassword
+              name="password"
+              control={form.control}
+              placeholder="Password"
+              disabled={pending}
+              outerElement={
+                <FieldDescription>
+                  <Link to="/forgot-password">Forgot Password?</Link>
+                </FieldDescription>
+              }
+            />
+
+            {!!error && <AlertError error={error} />}
+
+            <Button
+              size="lg"
+              className="w-full"
+              disabled={pending}
+              type="submit"
+            >
+              {pending ? <Spinner /> : "Log In"}
+            </Button>
+          </FieldGroup>
+        </form>
+      </CardContent>
+
+      <CardContent className="px-7 flex items-center justify-center">
+        <p>
+          Don&apos;t have an account?{" "}
+          <Link to="/signup">
+            <span className="text-primary">Sign Up</span>
+          </Link>
+        </p>
+      </CardContent>
+    </Card>
+  );
+}
