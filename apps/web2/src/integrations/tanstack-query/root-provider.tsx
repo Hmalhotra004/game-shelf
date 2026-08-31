4fb30e1f56f932b85d483070749b100552bb199d
Hmalhotra04 <hardikmalhotra150804@gmail.com>
2026-08-31 12:44:11 +0530

    feat(ui+auth): add logo, alert, auth client and routes updates

--- apps/web2/src/integrations/tanstack-query/root-provider.tsx
+++ apps/web2/src/integrations/tanstack-query/root-provider.tsx
@@ -1,10 +1,19 @@
-import { QueryClient } from '@tanstack/react-query'
+import { QueryClient } from "@tanstack/react-query";
 
 export function getContext() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 15,
+        gcTime: 1000 * 60 * 60 * 30,
+        retry: 1,
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
 
   return {
     queryClient,
-  }
+  };
 }
 export default function TanstackQueryProvider() {}
