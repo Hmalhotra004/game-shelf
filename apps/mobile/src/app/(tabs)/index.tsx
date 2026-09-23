b3f04967020814be76975de90eb810b3b80e3258
Hmalhotra04 <hardikmalhotra150804@gmail.com>
2026-09-23 18:06:00 +0530

    feat(ui wrap screen with ScreenWrapper component

--- apps/mobile/src/app/(tabs)/index.tsx
+++ apps/mobile/src/app/(tabs)/index.tsx
@@ -1,14 +1,13 @@
-import { Button } from "@/components/ui/button";
+import ScreenWrapper from "@/components/ui/screen-wrapper";
 import { Text } from "@/components/ui/text";
 import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
-      <Button>
-        <Text>hello</Text>
-      </Button>
-    </View>
+    <ScreenWrapper>
+      <View className="flex-1 items-center justify-center">
+        <Text>Home</Text>
+      </View>
+    </ScreenWrapper>
   );
 }
